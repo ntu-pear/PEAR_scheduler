2aef868c2568156889dc9f26e1aecabc5beccc36
CredoSpero <yixuan1402@gmail.com>
2023-12-27 14:01:07 +0800

    added code to insert schedule into schedule table

--- pear_schedule/api/routes.py
+++ pear_schedule/api/routes.py
@@ -1,5 +1,14 @@
 from flask import Blueprint, jsonify, current_app
+from pear_schedule.db_models.models import Schedule
 from pear_schedule.db_views.views import PatientsOnlyView, CompulsoryActivitiesOnlyView
+
+# IMPORT TEMPORARILY, WILL REFACTOR
+from sqlalchemy.orm import Session
+from pear_schedule.db import DB
+from pear_schedule.db_views.utils import compile_query
+from pear_schedule.utils import ConfigDependant
+import datetime
+##
 
 from pear_schedule.scheduler.groupScheduling import GroupActivityScheduler
 from pear_schedule.scheduler.compulsoryScheduling import CompulsoryActivityScheduler
@@ -12,7 +21,7 @@ blueprint = Blueprint("scheduling", __name__)
 @blueprint.route("/generate", methods=["GET"])
 def generate_schedule():
     config = current_app.config
-
+    
     # Set up patient schedule structure
     patientSchedules = {} # patient id: [[],[],[],[],[]]
 
@@ -34,11 +43,55 @@ def generate_schedule():
 
     # Schedule individual activities
     IndividualActivityScheduler.fillSchedule(patientSchedules)
-
-
+    
+    ## ------------------------------------------------------------------------------------------------
+    ## Inserting into Schedule Table
+    ## TODO: TRYING TO TEST TO INSERT TO TABLE... WILL REFACTOR
+    session = Session(bind=DB.engine)
+    latest_schedule_id = session.query(Schedule.ScheduleID).order_by(Schedule.ScheduleID.desc()).first()
+    new_schedule_id = latest_schedule_id[0] + 1
+    
+    print("Latest Schedule Id: ", latest_schedule_id[0])
+    
+    today = datetime.datetime.now()
+    start_of_week = today - datetime.timedelta(days=today.weekday())  # Monday
+    end_of_week = start_of_week + datetime.timedelta(days=4)  # Friday
+    
     for p, slots in patientSchedules.items():
         print(f"{p} Schedule: {slots}")
+        
+        days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
+        converted_schedule = {}
 
+        for i, day in enumerate(days):
+            activities = "-".join(slots[i])
+            converted_schedule[day] = activities
+            
+        new_schedule = Schedule(
+            ScheduleID=new_schedule_id,
+            PatientID=p,
+            StartDate=start_of_week,
+            EndDate=end_of_week,
+            Monday= converted_schedule["Monday"],
+            Tuesday= converted_schedule["Tuesday"],
+            Wednesday= converted_schedule["Wednesday"],
+            Thursday= converted_schedule["Thursday"],
+            Friday= converted_schedule["Friday"],
+            IsDeleted = 0,
+            CreatedDateTime = today,
+            UpdatedDateTime = today
+        )
+        
+        new_schedule_id = new_schedule_id + 1
+
+        # Add the Schedule object to the session and commit
+        session.add(new_schedule)
+        session.commit()
+        
+    # Close the session
+        session.close()
+    ## ------------------------------------------------------------------------------------------------
+        
     data = {"data": "Success"} 
     return jsonify(data) 
 
---
+++ pear_schedule/db_models/models.py
@@ -0,0 +1,23 @@
+from sqlalchemy import Column, Integer, DateTime, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class Schedule(Base):
+    __tablename__ = 'Schedule'
+
+    ScheduleID = Column(Integer, primary_key=True)
+    PatientID = Column(Integer)  
+    # PatientID = Column(Integer, ForeignKey('Patient.PatientID'))  
+
+    StartDate = Column(DateTime)
+    EndDate = Column(DateTime)
+    Monday = Column(String)
+    Tuesday = Column(String)
+    Wednesday = Column(String)
+    Thursday = Column(String)
+    Friday = Column(String)
+    
+    IsDeleted = Column(Integer)
+    CreatedDateTime = Column(DateTime)
+    UpdatedDateTime = Column(DateTime)
